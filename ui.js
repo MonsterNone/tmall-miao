01ce8ac3a65ac8a69da3ef63d5442296cb5de246
Monster None <alwaysalittlemonster@gmail.com>
2020-06-04 12:17:41 +0800

    增加交流群

--- ui.js
+++ ui.js
@@ -1,28 +1,37 @@
 "ui";
 
-const VERSION = 8
+const VERSION = 9
 
 ui.layout(
-    <vertical>
-        <button id="automationPermission" text="1. 授予无障碍权限" />
-        <button id="consolePermission" text="2. 授予悬浮窗权限" />
-        <button id="startTask" text="3. 开始每日任务" />
-        <button id="discountTask" text="4. 领取618红包（每天三次）" />
-        <button id="checkUpdate" text="检查更新" />
-        <text text="部分机型无障碍权限授予部分可能出现bug，请关闭软件重新打开授予权限。" textStyle="bold|italic" textColor="red" />
-        <text text="如果始终无法授予请重启手机尝试" />
-        <text text="使用说明" textColor="red" />
-        <text text="1. 运行脚本之前建议按首先点击授予权限" />
-        <text text="2. 脚本运行过程中按 音量减 即可强制停止" />
-        <text text="3. 部分每日任务需要手动完成" />
-        <text text="其他说明" textColor="red" />
-        <text text="1. 本脚本基于Auto.JS（感谢原开发者）" />
-        <text text="2. 免费！" />
-        <text autoLink="web" text="3. 项目地址https://github.com/MonsterNone/tmall-miao" />
-        <text text="4. 运行中出现bug请附上详细控制台log、页面截图等提交issue" />
-        <text text="5. 由于调用淘宝打开页面，部分手机管家可能会误报为诱导软件，实际上本软件绝无任何病毒行为" />
-        <text id="ver" line="2" />
-    </vertical>
+    <frame>
+        <vertical>
+            <button id="automationPermission" text="1. 授予无障碍权限" />
+            <button id="consolePermission" text="2. 授予悬浮窗权限" />
+            <button id="startTask" text="3. 开始每日任务" />
+            <button id="discountTask" text="4. 领取618红包（每天三次）" />
+            <button id="showQun" text="加入618列车组队交流群" />
+            <button id="checkUpdate" text="检查更新" />
+            <text text="部分机型无障碍权限授予部分可能出现bug，请关闭软件重新打开授予权限。" textStyle="bold|italic" textColor="red" />
+            <text text="如果始终无法授予请重启手机尝试" />
+            <text text="使用说明" textColor="red" />
+            <text text="1. 运行脚本之前建议按首先点击授予权限" />
+            <text text="2. 脚本运行过程中按 音量减 即可强制停止" />
+            <text text="3. 部分每日任务需要手动完成" />
+            <text text="其他说明" textColor="red" />
+            <text text="1. 本脚本基于Auto.JS（感谢原开发者）" />
+            <text text="2. 免费！" />
+            <text autoLink="web" text="3. 项目地址https://github.com/MonsterNone/tmall-miao" />
+            <text text="4. 运行中出现bug请附上详细控制台log、页面截图等提交issue" />
+            <text text="5. 由于调用淘宝打开页面，部分手机管家可能会误报为诱导软件，实际上本软件绝无任何病毒行为" />
+            <text id="ver" line="2" />
+        </vertical>
+        <vertical id="qun" visibility="gone" bg="#ffffff">
+            <img src="file://res/qun.png" />
+            <button id="hideQun" style="Widget.AppCompat.Button.Colored" text="隐藏" />
+            <text text="截图后打开微信扫描二维码加入" />
+            <text text="如果二维码无法进入请添加小助手微信拉进群：zs2020618" />
+        </vertical>
+    </frame>
 )
 
 ui.ver.setText('\n版本：V' + VERSION)
@@ -43,6 +52,14 @@ ui.startTask.click(function() {
 
 ui.discountTask.click(function() {
     engines.execScriptFile('./discount.js')
+})
+
+ui.showQun.click(function() {
+    ui.qun.visibility = 0 
+})
+
+ui.hideQun.click(function() {
+    ui.qun.visibility = 8
 })
 
 ui.checkUpdate.click(function() {
