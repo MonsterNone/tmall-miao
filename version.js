1cb4284aa86e1b0c3a5ff4379a6ab5ecd0946864
MonsterNone <alwaysalittlemonster@gmail.com>
2022-05-28 22:23:40 +0800

    V12

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 var version = {}
-version.VERSION = '2022618-11'
+version.VERSION = '2022618-12'
 version.showVersion = function () {
     console.log('当前版本：'+version.VERSION)
     toast('当前版本：'+version.VERSION)
