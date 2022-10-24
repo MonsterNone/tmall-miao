e5ddb3016486f6622c1cf0b5f239a8a83260a88f
Find-fengqingwu <40506441+Find-fengqingwu@users.noreply.github.com>
2022-10-24 22:33:19 +0800

    修复商品数量不足bug

--- tb_nengliang.js
+++ tb_nengliang.js
@@ -299,7 +299,7 @@ try {
                 console.log('商品数量不足，向下翻页', buttons.length)
                 scrollDown()
                 sleep(2000)
-                buttons = text('马上抢').find()
+                buttons = textMatches(/.*马上抢.*|.*付定随机.*|.*立付.*/).find()
             }
             if (count > buttons.length) {
                 throw '商品数量不足，退出任务'
